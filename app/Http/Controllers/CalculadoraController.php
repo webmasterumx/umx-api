dcce42994c187c7c8ee00a6754c6d591d4eee640
WebMaster <umrec_web@unimex.edu.mx>
2022-09-10 12:10:26 -0500

    add calculadora controller

---
+++ app/Http/Controllers/CalculadoraController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Artisaninweb\SoapWrapper\SoapWrapper;
+use Illuminate\Http\Request;
+
+class CalculadoraController extends Controller
+{
+    protected $soapWrapper;
+
+    public function __construct(SoapWrapper $soapWrapper)
+    {
+        $this->soapWrapper = $soapWrapper;
+        
+        $this->soapWrapper->add( 'Calculadora', function($service){
+    
+            $service
+            ->wsdl( 'http://comunimex.lat/TestingWSOperacionesUnificadas/calculadoracuotas.asmx?WSDL' )
+            ->trace( TRUE );
+    
+        });
+
+    }
+}
