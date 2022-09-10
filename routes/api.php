29847d8ec96c9b110db7892c655912058821a685
WebMaster <umrec_web@unimex.edu.mx>
2022-09-10 11:54:54 -0500

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\PaginaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(PaginaController::class)->group( function(){
+
+    Route::get('pagina/planteles', 'getPlanteles');
+    Route::post('pagina/niveles/{plantel}', 'getNiveles')->where('plantel', '[2-5]+' );
+    Route::post('pagina/periodos/{plantel}', 'getPeriodos')->where('plantel', '[2-5]+' );
+    Route::post('pagina/carreras/{plantel}/{nivel}/{periodo}', 'getCarreras')->where([
+        'plantel' => '[2-5]+',
+        'nivel'   => '[1-3]+',
+    ])->whereNumber('perido');
+
+    Route::post('pagina/turnos/{plantel}/{nivel}/{periodo}/{carrera}', 'getTurnos')->where([
+        'plantel' => '[2-5]+',
+        'nivel'   => '[1-3]+',
+    ])->whereNumber('perido')->whereNumber('carrera');
+
+});
+
+
