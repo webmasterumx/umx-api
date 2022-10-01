4a02fdc309a43515cbdbeaccc1353f04be955ad1
WebMaster <umrec_web@unimex.edu.mx>
2022-10-01 13:16:40 -0500

    add header

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,6 +17,9 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         //return $next($request);
-        return $next($request)->header('Access-Control-Allow-Origin', '*');
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', '*');
     }
 }
