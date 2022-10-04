daa87c331b6ab9aa8b26db9ba9323cd2dd7a4081
WebMaster <umrec_web@unimex.edu.mx>
2022-10-04 09:51:18 -0500

    return next($request)

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -23,12 +23,13 @@ class Cors
             'Access-Control-Allow-Headers' => '*'
         ];
         
-        
+        /*
         return $next($request)
         ->header('Access-Control-Allow-Origin', "*")
         ->header('Access-Control-Allow-Methods', "HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS")
         ->header('Access-Control-Allow-Headers', "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization")
         ->header('Content-Type', 'application/json');
+        */
 
 
 
@@ -39,6 +40,7 @@ class Cors
             ->header('Access-Control-Allow-Headers', "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization")
             ->header('HTTP/1.1 200 OK');
         }
+        return $next($request);
         
     }
 }
