de12c8cb9fda17605f52d579159238723a4c3e3f
WebMaster <umrec_web@unimex.edu.mx>
2022-10-04 09:52:06 -0500

    set exposed to *

--- config/cors.php
+++ config/cors.php
@@ -23,11 +23,11 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
