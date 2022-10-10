d3fefd8f1932c7716a3c6c462234c46db2392ee1
WebMaster <umrec_web@unimex.edu.mx>
2022-10-10 18:21:31 -0500

    add handle cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,33 +14,12 @@ class Cors
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
-    {
-        //return $next($request);
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => '*'
-        ];
+    public function handle(Request $request, Closure $next){
         
-        /*
         return $next($request)
-        ->header('Access-Control-Allow-Origin', "*")
-        ->header('Access-Control-Allow-Methods', "HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS")
-        ->header('Access-Control-Allow-Headers', "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization")
-        ->header('Content-Type', 'application/json');
-        */
-
-
-
-        if( $request->isMethod('OPTIONS') ){
-            //return response()->json('OK', 200, $headers); 
-            return $next($request)
-            ->header('Access-Control-Allow-Origin', "*")
-            ->header('Access-Control-Allow-Headers', "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization")
-            ->header('HTTP/1.1 200 OK');
-        }
-        return $next($request);
-        
+        ->header("Access-Control-Allow-Origin", "*")
+        ->header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+        ->header("Access-Control-Allow-Headers", "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    
     }
 }
