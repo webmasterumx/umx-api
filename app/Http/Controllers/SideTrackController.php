51ed757360a2ee9f0ec12d6fc3f8c8b6a5e9b168
WebMaster <umrec_web@unimex.edu.mx>
2022-10-13 12:40:24 -0500

    add functions

--- app/Http/Controllers/SideTrackController.php
+++ app/Http/Controllers/SideTrackController.php
@@ -3,8 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Artisaninweb\SoapWrapper\SoapWrapper;
+use Illuminate\Support\Facades\App;
 
 class SideTrackController extends Controller
 {
-    //
+    protected $soapWrapper;
+    protected $url;
+    protected $mensaje;
+
+    public function __construct(SoapWrapper $soapWrapper)
+    {
+        App::environment('local') ? 
+            $this->url = "http://comunimex.lat/TestingWSOperacionesUnificadas/SideTrack.asmx?WSDL" :
+            $this->url = "http://comunimex.lat/WSOperacionesUnificadas/SideTrack.asmx?WSDL";
+
+        $this->soapWrapper = $soapWrapper;
+        $this->mensaje = [
+            "error" => "No hay datos disponibles"
+        ];
+        
+        $this->soapWrapper->add( 'SideTrack', function($service){
+            $service->wsdl( $this->url )->trace( TRUE );
+        });
+    }
+
+    public function getCarreras( Request $request){
+
+        $params= json_decode($request->getContent(), true);
+        $carreras = $this->soapWrapper->call('SideTrack.ObtenerCatalogoCarreras', [ $params ]);
+        $respuesta = $carreras->ObtenerCatalogoCarrerasResult;
+
+        if( empty($respuesta) || empty($respuesta->CarerrasDTO) ) return response()->json($this->mensaje, 400);
+        return response()->json( $respuesta->CarerrasDTO);
+
+    }
+
+    public function getTurnos( Request $request){
+
+        $params= json_decode($request->getContent(), true);
+        $turnos = $this->soapWrapper->call('SideTrack.ObtenerCatalogoTurnos', [ $params ]);
+        $respuesta = $turnos->ObtenerCatalogoTurnosResult;
+
+        if( empty($respuesta) || empty($respuesta->TurnosDTO) ) return response()->json($this->mensaje, 400);
+        return response()->json( $respuesta->TurnosDTO);
+
+    }
+
+
 }
