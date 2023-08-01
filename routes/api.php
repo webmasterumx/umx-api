62a968235da46e9f84e67d6f618efece4b4f8279
WebMaster <umrec_web@unimex.edu.mx>
2023-08-01 09:44:48 -0600

    se crea ruta para obtener archivos

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\PreinscipcionController;
 use App\Http\Controllers\SideTrackController;
 use App\Http\Controllers\GraduacionesController;
 use App\Http\Controllers\KontuxController;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -105,4 +106,16 @@ Route::controller( KontuxController::class )->group( function(){
     Route::post('kontux/carreras', 'getCarreras');
     Route::post('kontux/turnos', 'getTurnos');
     Route::post('kontux/valida-celular', 'validaCelular');
+    Route::get('kontux/{tipo}/{plantel}/{name}', function($tipo, $plantel, $name){
+        if($tipo == 'archivos'){
+            $file = Storage::disk('public')->get($plantel.'/'.$name.'.pdf');
+            return( response($file, 200))->header('Content-Type', 'application/pdf');
+        }else if($tipo == 'videos'){
+            $file = Storage::disk('public')->get($plantel.'/'.$name.'.mp4');
+            return( response($file, 200))->header('Content-Type', 'video/mp4');
+        }else {
+            return response(['error' => 'archivo no encontrado'], 400);
+        }
+    });
+    Route::post('kontux/documentos', 'getFiles');
 });
