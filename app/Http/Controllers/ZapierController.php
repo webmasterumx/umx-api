18ab95db95f34bc9e35ed3ecd44710d35b74b7f3
WebMaster <umrec_web@unimex.edu.mx>
2023-08-19 11:16:11 -0600

    add adview ws route

--- app/Http/Controllers/ZapierController.php
+++ app/Http/Controllers/ZapierController.php
@@ -14,13 +14,13 @@ class ZapierController extends Controller
 
     public function __construct( SoapWrapper $soapWrapper) {
 
-        $this->baseUrl = config('ws.url');
-        $this->url = $this->baseUrl."Kontux.asmx?WSDL";
+        $this->baseUrl = config('app.ws_url');
+        $this->url = $this->baseUrl."Adview.asmx?WSDL";
         $this->soapWrapper = $soapWrapper;
         $this->mensaje = [
             "error" => "No hay datos disponibles"
         ];
-        $this->soapWrapper->add('KNTX', function( $service ){
+        $this->soapWrapper->add('ADVW', function( $service ){
             $service->wsdl( $this->url)
                     ->trace( TRUE );
         });
@@ -30,9 +30,13 @@ class ZapierController extends Controller
     public function facebookRegister( Request $request ){
 
         $params = $request->all();
+        $addFacebookProspecto = $this->soapWrapper->call('ADVW.AgregaProspectoCrmFacebook', [$params]);
+        $respuesta = $addFacebookProspecto->AgregaProspectoCrmFacebookResult;
 
-        if( empty($params) ) return response()->json($this->mensaje, 400);
-        return response()->json( $params );
+        if( empty($respuesta) || $respuesta == 0  ) return response()->json($this->mensaje, 400);
+        return response()->json([
+            'respuesta' => $respuesta
+        ]);
 
     }
 
